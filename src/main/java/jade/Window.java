e08f178d0655a97068385c420b3518d2a7b454b6
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2023-04-17 15:05:19 -0500

    Tutorial #11 nah bruh nah nah nah nah nah nah nah nah nah nah nah nah nah nah!...
    16:00 seconds

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -71,6 +71,10 @@ public class Window
         return Window.window;
     }
 
+    public static Scene getScene() {
+        return get().currentScene;
+    }
+
     public void run()
     {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
---
+++ src/main/java/renderer/RenderBatch.java
@@ -0,0 +1,46 @@
+package renderer;
+
+import components.SpriteRenderer;
+
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+
+public class RenderBatch {
+    // Vertex
+    // ======
+    // Pos               color
+    // float, float      float, float, float, float
+    private final int POS_SIZE = 2;
+    private final int COLOR_SIZE = 4;
+
+    private final int POS_OFFSET = 0;
+    private final int COLOR_OFFSET = POS_OFFSET + POS_SIZE * Float.BYTES;
+    private final int VERTEX_SIZE = 6;
+    private final int VErTEX_SIZE_BYTES = VERTEX_SIZE * Float.BYTES;
+
+    private SpriteRenderer[] sprites;
+    private int numSprites;
+    private boolean hasRoom;
+    private float[] vertices;
+
+    private int vaoID, vboID;
+    private int maxBatchSize;
+    private Shader shader;
+
+    public RenderBatch(int maxBatchSize) {
+        shader = new Shader("assets/shaders/default.glsl");
+        shader.compile();
+        this.sprites = new SpriteRenderer[maxBatchSize];
+        this.maxBatchSize = maxBatchSize;
+
+        // 4 vertices quads
+        vertices = new float[maxBatchSize * 4 * VERTEX_SIZE];
+
+        this.numSprites = 0;
+        this.hasRoom = true;
+    }
+
+    public void start() {
+        // Generate and bind a Vertex Array Object
+        vaoID = glGenVertexArrays();
+    }
+}
